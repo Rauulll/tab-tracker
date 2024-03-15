e781cf37f760c745d866366b07ee8907e5d69cfa
Rauulll <nyaokeraul003@gmail.com>
2024-03-15 22:49:23 +0300

    adding an endpoint /app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
 console.log('hello world')
+
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const express = require('express')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/status', (_req, res) => {
+  res.send({ message: 'hello world' })
+})
+
+app.listen(process.env.PORT || 8801)
